7d17c78d85da2ffc11e74a1bc49919ad277ec010
Jeffrey Gilbert <visualjeff@linux.com>
2017-05-25 18:11:43 -0700

    have working tests

---
+++ test/index.js
@@ -0,0 +1,120 @@
+'use strict';
+
+const MsgPackPlugin = require('../');
+
+const Hapi = require('hapi');
+const Code = require('code');
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
+
+const describe = lab.experiment;
+const expect = Code.expect;
+const it = lab.test;
+
+const msgpack = require('msgpack-lite');
+
+describe('msgpack-plugin', () => {
+
+    it('loads as a plugin', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+        server.register([MsgPackPlugin], (err) => {
+
+            expect(err).to.not.exist();
+            done();
+        });
+    });
+
+    it('user request without proper accept header', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+        server.register([MsgPackPlugin], (err) => {
+
+            expect(err).to.not.exist();
+        });
+
+        server.route([{
+            method: 'GET',
+            path: '/api/user',
+            handler: (request, reply) => {
+
+                reply({
+                    statusCode: 200,
+                    message: 'Getting All User Data',
+                    data: [{
+                        name: 'Kashish',
+                        age: 24
+                    }, {
+                        name: 'Shubham',
+                        age: 21
+                    }, {
+                        name: 'Jasmine',
+                        age: 24
+                    }]
+                });
+            }
+        }]);
+        const options = {
+            method: 'GET',
+            url: '/api/user',
+            headers: {}
+        };
+
+        server.inject(options, (res) => {
+
+            expect(res.result).to.exist();
+            expect(res.result.statusCode).to.equal(200);
+            expect(res.result.message).to.equal('Getting All User Data');
+            expect(res.result.data).to.equal([{
+                name: 'Kashish',
+                age: 24
+            }, {
+                name: 'Shubham',
+                age: 21
+            }, {
+                name: 'Jasmine',
+                age: 24
+            }]);
+            done();
+        });
+    });
+
+    it('user request with proper accept header', (done) => {
+
+        const server = new Hapi.Server();
+        server.connection();
+        server.register([MsgPackPlugin], (err) => {
+
+            expect(err).to.not.exist();
+        });
+
+        server.route([{
+            method: 'GET',
+            path: '/api/user',
+            handler: (request, reply) => {
+
+                reply({statusCode: 200, message: 'Getting All User Data', data: [{ name: 'Kashish', age: 24 }, { name: 'Shubham', age: 21 }, { name: 'Jasmine', age: 24 }] });
+	    }
+	}
+	]);
+
+        const options = {
+            method: 'GET',
+            url: '/api/user',
+            headers: {
+                accept: 'application/x-msgpack',
+            }
+        };
+
+        server.inject(options, (res) => {
+
+            expect(res.result).to.exist();
+	    expect(res.statusCode).to.equal(200);
+	    expect(res.headers['content-type']).to.equal('application/x-msgpack');
+	    expect(JSON.stringify(msgpack.decode(res.rawPayload))).to.equal('{"statusCode":200,"message":"Getting All User Data","data":[{"name":"Kashish","age":24},{"name":"Shubham","age":21},{"name":"Jasmine","age":24}]}');
+            done();
+        });
+    });
+});
